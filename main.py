89803e6772312ddb39936ecfc9263fef52eb90a4
Hugo Milenkovic <hugo.milenkovic@gmail.com>
2022-08-14 03:19:25 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+continuer = True
+
+while continuer:
+    choice = input("Choose Rock, Paper or Cisors: ")
+    computer = ""
+    # radnom computer
+
+    if choice == "Rock":
+        if computer == "Rock":
+    elif choice == "Paper":
+        pass
+    elif choice =="Cisors":
+        pass
+    else:
+        print("Error")
