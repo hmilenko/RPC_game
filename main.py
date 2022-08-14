5adcf9e6d0f99a8b721c99c1b972cddd49b1f997
hmilenko <hugo.milenkovic@gmail.com>
2022-08-14 03:43:29 +0200

    last UPDATE

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+from logging import raiseExceptions
 import random
+from time import sleep
+
+def result_display():
+    print("3...", end="")
+    sleep(1)
+    print("2...", end="")
+    sleep(1)
+    print("1...")
+    sleep(1)
 
 while True:
     user_action = input("Enter a choice (rock, paper, scissors): ")
+    result_display()
     possible_actions = ["rock", "paper", "scissors"]
     computer_action = random.choice(possible_actions)
     print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+    bypass = False
 
     if user_action == computer_action:
         print(f"Both players selected {user_action}. It's a tie!")
@@ -23,7 +35,11 @@ while True:
             print("Scissors cuts paper! You win!")
         else:
             print("Rock smashes scissors! You lose.")
-
-    play_again = input("Play again? (y/n): ")
-    if play_again.lower() != "y":
-        break
+    else:
+        print("Choice not valid")
+        bypass = True
+        
+    if not bypass:
+        play_again = input("Play again? (y/n): ")
+        if play_again.lower() != "y":
+            break
